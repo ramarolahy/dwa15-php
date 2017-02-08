d97ffab6699b5411ba6c20d35fc12cdbed5e4345
Susan Buck <susan@susanbuck.net>
2017-02-08 00:22:48 -0500

    Tools convenience methods

--- dump.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function dump($mixed = null) {
-    echo '<pre>';
-    var_dump($mixed);
-    echo '</pre>';
-}
---
+++ tools.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+*
+*/
+function dump($mixed = null) {
+    echo '<pre>';
+    var_dump($mixed);
+    echo '</pre>';
+}
+
+/**
+* 
+*/
+function sanitize($mixed = null) {
+
+    # Base case
+    if(!is_array($mixed)) {
+        return htmlentities($mixed, ENT_QUOTES, "UTF-8");
+    }
+    else {
+        return sanitize(array_shift($mixed));
+    }
+
+    return $mixed;
+
+}

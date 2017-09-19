bdd71b6937f023190deefa55491827ca8a8a8f82
Susan Buck <susan@susanbuck.net>
2017-09-19 15:27:28 -0400

    Include unideal example commented out

--- displayVsLogic/logic.php
+++ displayVsLogic/logic.php
@@ -10,3 +10,15 @@ if (isset($_GET['answer'])) {
         $correct = false;
     }
 }
+
+
+// We want to avoid this:
+// if (isset($_GET['answer'])) {
+//     $answer = strtolower($_GET['answer']);
+//
+//     if (in_array($answer, ['red', 'yellow', 'green'])) {
+//         $results = '<div class='positive'><i class="fa fa-smile-o"></i> '.sanitize($answer).' is correct!';
+//     } else {
+//         $results = '<div class='negative'><i class="fa fa-frown-o"></i> '.sanitize($answer).' is incorrect; please try again.';
+//     }
+// }

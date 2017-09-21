e0d1e3e7384b79ed8fa4b31a66716b710f79544e
Susan Buck <susan@susanbuck.net>
2017-09-21 12:17:02 -0400

    Cipher examples

---
+++ cipher/examples.php
@@ -0,0 +1,18 @@
+<?php
+require('helpers.php');
+require('Cipher.php');
+
+$cipherA = new Cipher('shift');
+dump('Hello World = '.$cipherA->encode('Hello World'));
+dump('zoo = '.$cipherA->encode('zoo'));
+
+$cipherB = new Cipher('vowels');
+dump('Hello World = '.$cipherB->encode('Hello World'));
+dump('zoo = '.$cipherB->encode('zoo'));
+
+# Cipher method does not exist
+try {
+    $cipherB = new Cipher('xyz');
+} catch (Exception $e) {
+    dump($e->getMessage());
+}

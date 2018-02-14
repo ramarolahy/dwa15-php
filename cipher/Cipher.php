3fcece8621c60052d4cfe09f0b08829b8702b229
Susan Buck <susan@susanbuck.net>
2018-02-14 14:26:09 -0500

    Misc. formatting clean-up

--- cipher/Cipher.php
+++ cipher/Cipher.php
@@ -6,57 +6,52 @@ class Cipher
 {
 
     /**
-    * Properties
-    */
+     * Properties
+     */
     private $algorithm; # What cipher algorithm to use (e.g. vowels, shift)
     private $algorithms = ['vowels', 'shift']; # Array of available algorithms
     private $alphabet; # Array of letters a-z
     private $vowels; # Array of vowels
 
     /**
-    * Magic method that's invoked whenever an object of this class is instantiated
-    */
+     * Magic method that's invoked whenever an object of this class is instantiated
+     */
     public function __construct($algorithm = 'vowels')
     {
-
         # Set alphabet and vowels class properties
         $this->alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
         $this->vowels = str_split('aeiou');
 
         # Confirm the cipher method exists
         if (!in_array($algorithm, $this->algorithms)) {
-            throw new \Exception("Cipher algorithm `".$algorithm."` not found", 1);
+            throw new \Exception("Cipher algorithm `" . $algorithm . "` not found", 1);
         }
 
         # Set algorithm class property
         $this->algorithm = $algorithm;
     }
 
-
     /**
-    *
-    */
+     *
+     */
     public function encode(String $input)
     {
         return $this->process($input, 'encode');
     }
 
-
     /**
-    *
-    */
+     *
+     */
     public function decode(String $input)
     {
         return $this->process($input, 'decode');
     }
 
-
     /**
-    *
-    */
+     *
+     */
     private function process(String $input, $encodeOrDecode)
     {
-
         $results = '';
 
         foreach (str_split($input) as $character) {
@@ -64,7 +59,7 @@ class Cipher
 
             # Only encode letters; skip over spaces, symbols, etc.
             if (ctype_alnum($character)) {
-                $algorithm = $this->algorithm.$encodeOrDecode;
+                $algorithm = $this->algorithm . $encodeOrDecode;
                 $character = $this->$algorithm($character);
             }
 
@@ -74,14 +69,12 @@ class Cipher
         return $results;
     }
 
-
     /**
-    * Each letter is replaced with its adjacent letter in the alphabet;
-    * e.g. a becomes b, x becomes y, z becomes a, etc.
-    */
+     * Each letter is replaced with its adjacent letter in the alphabet;
+     * e.g. a becomes b, x becomes y, z becomes a, etc.
+     */
     private function shiftEncode(String $letter)
     {
-
         if (ctype_digit($letter)) {
             return $letter;
         }
@@ -97,13 +90,11 @@ class Cipher
         return $this->alphabet[$position];
     }
 
-
     /**
-    *
-    */
+     *
+     */
     private function shiftDecode(String $letter)
     {
-
         if (ctype_digit($letter)) {
             return $letter;
         }
@@ -119,14 +110,11 @@ class Cipher
         return $this->alphabet[$position];
     }
 
-
     /**
-    *
-    * vowels are replaced with their numerical equivalent where a=0, i=1, o=2, etc.
-    */
+     * vowels are replaced with their numerical equivalent where a=0, i=1, o=2, etc.
+     */
     private function vowelsEncode(String $letter)
     {
-
         if (in_array($letter, $this->vowels)) {
             return array_search($letter, $this->vowels);
         } else {
@@ -134,13 +122,11 @@ class Cipher
         }
     }
 
-
     /**
-    *
-    */
+     *
+     */
     private function vowelsDecode(String $letter)
     {
-
         if (isset($this->vowels[$letter])) {
             return $this->vowels[$letter];
         } else {

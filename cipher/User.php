040abaf319d682d9987daf47e70697235adbfc2b
Susan Buck <susan@susanbuck.net>
2018-02-14 14:26:43 -0500

    User example discussed in Week 4, Part 2

---
+++ cipher/User.php
@@ -0,0 +1,21 @@
+<?php
+
+class User {
+
+    private $password;
+
+    public function __construct() {
+        # Generate a random string for the password
+        $this->password = substr(md5(rand()), 0, 7);
+    }
+
+    public function getPassword() {
+        return hash('sha256', $this->password);
+    }
+}
+
+/*
+Example usage:
+$user = new User();
+echo $user->getPassword();
+*/

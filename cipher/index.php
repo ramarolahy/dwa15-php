13cb79d17bfeb7ddb7006d3898043d848e769777
Susan Buck <susan@susanbuck.net>
2018-02-13 19:39:09 -0500

    Updates to Cipher for Spring 2018

--- cipher/index.php
+++ cipher/index.php
@@ -7,48 +7,52 @@ require('logic.php');
 <head>
     <title>Secret Cipher</title>
     <meta charset='utf-8'>
+
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+          rel='stylesheet' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
+          crossorigin='anonymous'>
     <link href='css/styles.css' rel='stylesheet'>
 </head>
 <body>
 
-    <h1>Secret Cipher</h1>
+<h1>Secret Cipher</h1>
 
-    <p id='definition'>
-        <strong>ci·pher</strong>: secret or disguised way of writing; a code.
-    </p>
+<p id='definition'>
+    <strong>ci·pher</strong>: secret or disguised way of writing; a code.
+</p>
 
-    <form method='GET'>
+<form method='GET'>
 
-        <div class='form-group'>
-            <label for='message'>Your message:</label><br>
-            <textarea name='message' id='message'>Hello World</textarea>
-        </div>
+    <div class='form-group'>
+        <label for='message'>Your message:</label>
+        <textarea name='message' id='message'>Hello World</textarea>
+    </div>
 
-        <div class='form-group'>
-            <label>Choose your encoding type...</label><br>
+    <div class='form-group'>
+        <h2>Choose your encoding type...</h2>
 
-            <input type='radio' name='type' value='vowels' <?=$checkVowels?>>
+        <input type='radio'
+               name='type'
+               value='vowels' <?= (empty($_GET) or $_GET['type'] == 'vowels') ? 'checked' : '' ?>>
+        <label for='vowels'>Vowel replacement</label>
+        <p>Vowels are replaced with their numerical equivalent where a=0, i=1, o=2, etc.</p>
 
-            <label for='vowels'>Vowel replacement</label>
-            <p>Vowels are replaced with their numerical equivalent where a=0, i=1, o=2, etc.</p>
+        <input type='radio'
+               name='type'
+               value='shift' <?= (isset($_GET['type']) and $_GET['type'] == 'shift') ? 'checked' : '' ?>>
+        <label for='vowels'>Shift letters</label>
+        <p>Each letter is replaced with its adjacent letter in the alphabet; e.g. a becomes b, x becomes y, z becomes a, etc.</p>
+    </div>
 
-            <input type='radio' name='type' value='shift' <?=$checkShift?>>
-            <label for='vowels'>Shift letters</label>
-            <p>
-                Each letter is replaced with its adjacent letter in the alphabet;
-                e.g. a becomes b, x becomes y, z becomes a, etc.
-            </p>
-        </div>
+    <input type='submit' value='Encode' class='btn btn-primary'>
+</form>
 
-        <input type='submit' value='Encode'>
-    </form>
+<?php if (isset($results)) : ?>
+    <h2>Results:</h2>
+    <div id='results'>
+        <?= sanitize($results) ?>
+    </div>
+<?php endif ?>
 
-    <?php if (isset($results)) : ?>
-        <h2>Results:</h2>
-        <div id='results'>
-            <?=sanitize($results)?>
-        </div>
-    <?php endif ?>
-
-    </body>
-    </html>
+</body>
+</html>
--- cipher/logic.php
+++ cipher/logic.php
@@ -3,20 +3,10 @@ require('Cipher.php');
 
 use DWA\Cipher;
 
-# Used in the display to check the appropriate radio button
-$checkVowels = 'CHECKED';
-$checkShift = '';
-
 if (!empty($_GET)) {
     # Get data from the form
     $message = $_GET['message'];
     $type = $_GET['type'];
-
-    if ($type == 'vowels') {
-        $checkVowels = 'CHECKED';
-    } else {
-        $checkShift = 'CHECKED';
-    }
 
     # Instantiate a new Cipher object
     $cipher = new Cipher($type);
